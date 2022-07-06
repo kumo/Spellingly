1a851bcef7f0358456c7f4ff5eb1c4794305c1c4
Robert Clarke <kumo@kumo.it>
2022-07-07 00:00:30 +0200

    Initial Commit

---
+++ Lettery/LetteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetteryApp.swift
+//  Lettery
+//
+//  Created by Robert Clarke on 07/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LetteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
