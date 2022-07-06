e8f14581f2f8c4c5d053b4cd97b50346e0e6ba3a
Robert Clarke <kumo@kumo.it>
2022-07-07 00:34:18 +0200

    Mockup of how the spelling and letters could appear

--- Lettery/ContentView.swift
+++ Lettery/ContentView.swift
@@ -9,8 +9,194 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+        HStack {
+            VStack {
+                Text("F")
+                    .font(.largeTitle)
+                Text("Foxtrot")
+                    .font(.caption)
+            }
+            VStack {
+                Text("O")
+                    .font(.largeTitle)
+                Text("Oscar")
+                    .font(.caption)
+            }
+            VStack {
+                Text("X")
+                    .font(.largeTitle)
+                Text("X-Ray")
+                    .font(.caption)
+            }
+            VStack {
+                Text("T")
+                    .font(.largeTitle)
+                Text("Tango")
+                    .font(.caption)
+            }
+            VStack {
+                Text("R")
+                    .font(.largeTitle)
+                Text("Romeo")
+                    .font(.caption)
+            }
+            VStack {
+                Text("O")
+                    .font(.largeTitle)
+                Text("Oscar")
+                    .font(.caption)
+            }
+            VStack {
+                Text("T")
+                    .font(.largeTitle)
+                Text("Tango")
+                    .font(.caption)
+            }
+        }
+        .padding(10.0)
+            Spacer()
+            HStack {
+                VStack {
+                    Text("Foxtrot")
+                        .font(.caption)
+                    Text("F")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("Oscar")
+                        .font(.caption)
+                    Text("O")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("X-Ray")
+                        .font(.caption)
+                    Text("X")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("Tango")
+                        .font(.caption)
+                    Text("T")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("Romeo")
+                        .font(.caption)
+                    Text("R")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("Oscar")
+                        .font(.caption)
+                    Text("O")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("Tango")
+                        .font(.caption)
+                    Text("T")
+                        .font(.largeTitle)
+                }
+
+            }
+            .padding(10.0)
+            
+            HStack {
+                VStack {
+                    Text("At")
+                        .font(.caption)
+                    Text("@")
+                        .font(.largeTitle)
+                }
+
+            }
+            .padding(10.0)
+
+            HStack {
+                VStack {
+                    Text("Foxtrot")
+                        .font(.caption)
+                    Text("F")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("Oscar")
+                        .font(.caption)
+                    Text("O")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("X-Ray")
+                        .font(.caption)
+                    Text("X")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("Tango")
+                        .font(.caption)
+                    Text("T")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("Romeo")
+                        .font(.caption)
+                    Text("R")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("Oscar")
+                        .font(.caption)
+                    Text("O")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("Tango")
+                        .font(.caption)
+                    Text("T")
+                        .font(.largeTitle)
+                }
+
+            }
+            .padding(10.0)
+
+            HStack {
+                VStack {
+                    Text("Dot")
+                        .font(.caption)
+                    Text(".")
+                        .font(.largeTitle)
+                }
+
+            }
+            .padding(10.0)
+
+
+            HStack {
+                VStack {
+                    Text("Charlie")
+                        .font(.caption)
+                    Text("C")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("Oscar")
+                        .font(.caption)
+                    Text("O")
+                        .font(.largeTitle)
+                }
+                VStack {
+                    Text("Mike")
+                        .font(.caption)
+                    Text("M")
+                        .font(.largeTitle)
+                }
+            }
+            .padding(10.0)
+
+        }
+        .padding(10.0)
     }
 }
 
