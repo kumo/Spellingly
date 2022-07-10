076e951ba8112e750b3a2a56313661d595e4f293
Robert Clarke <kumo@kumo.it>
2022-07-10 23:06:52 +0200

    Adding some disabled options to the settings

--- Lettery/SettingsView.swift
+++ Lettery/SettingsView.swift
@@ -9,24 +9,33 @@ import SwiftUI
 
 struct SettingsView: View {
     @AppStorage("showCapitalsKey") var showCapitals: Bool = false
+    @AppStorage("removeLeadingSpacesKey") var removeLeadingSpace: Bool = false
+    @AppStorage("removeTrailingLettersKey") var startOnNewLine: Bool = false
 
     var body: some View {
         Form {
             Section(header: Text("Preview")) {
                 LetterGrid(
-                    letters: "Here and now".enumerated(),
+                    letters: "Then to now".enumerated(),
                     converter: Converter(),
                     columns: Array(repeating: GridItem.init(.flexible()), count: 4))
             }
 
             Section(header: Text("Options")) {
                 Toggle("Show capital letters", isOn: $showCapitals)
+                Toggle("Remove leading spaces", isOn: $removeLeadingSpace)
+                    .disabled(true)
+                Toggle("Start words on a new line", isOn: $startOnNewLine)
+                    .disabled(true)
             }
             
             Section(header: Text("About"), footer: Text("Version 1.0.0")) {
                 Label("Tip Jar", systemImage: "heart.text.square.fill")
+                    .disabled(true)
                 Label("Bookmarks", systemImage: "bookmark.circle.fill")
+                    .disabled(true)
                 Label("Support", systemImage: "link.circle.fill")
+                    .disabled(true)
             }
         }
     }
