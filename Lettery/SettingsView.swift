c54ea77c74adbf46a21760782dbb2f25fe6206e6
Robert Clarke <kumo@kumo.it>
2022-07-10 22:30:41 +0200

    Playing with about menu

--- Lettery/SettingsView.swift
+++ Lettery/SettingsView.swift
@@ -18,8 +18,15 @@ struct SettingsView: View {
                     converter: Converter(),
                     columns: Array(repeating: GridItem.init(.flexible()), count: 4))
             }
+
             Section(header: Text("Options")) {
                 Toggle("Show capital letters", isOn: $showCapitals)
+            }
+            
+            Section(header: Text("About"), footer: Text("Version 1.0.0")) {
+                Label("Tip Jar", systemImage: "heart.text.square.fill")
+                Label("Bookmarks", systemImage: "bookmark.circle.fill")
+                Label("Support", systemImage: "link.circle.fill")
             }
         }
     }
