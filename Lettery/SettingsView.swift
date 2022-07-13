9fcb4f5983ee0c32c27701a08d60c0f0e7f93a1f
Robert Clarke <kumo@kumo.it>
2022-07-13 23:16:31 +0200

    Loading current converter in settings

--- Lettery/SettingsView.swift
+++ Lettery/SettingsView.swift
@@ -8,20 +8,27 @@
 import SwiftUI
 
 struct SettingsView: View {
+
+    // MARK: - Properties
     @AppStorage("showCapitalsKey") var showCapitals: Bool = false
     @AppStorage("showCapitalSpellingsKey") var showCapitalSpellings: Bool = false
     @AppStorage("showSpellingOnTopKey") var showSpellingOnTop: Bool = true
     @AppStorage("removeLeadingSpacesKey") var removeLeadingSpace: Bool = false
     @AppStorage("removeTrailingLettersKey") var startOnNewLine: Bool = false
-    
+    @AppStorage("converterNameKey") var converterName: String = "NATO"
+    @State private var converter: Converter?
+
+    // MARK: - UI Elements
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Preview")) {
-//                    LetterGrid(
-//                        letters: "Then to now".enumerated(),
-//                        converter: BuiltInConverter(),
-//                        columns: Array(repeating: GridItem.init(.flexible()), count: 4))
+                Section(header: Text("Preview - \(converterName)")) {
+                    if let converter = converter {
+                        LetterGrid(
+                            letters: "Then to now".enumerated(),
+                            converter: converter,
+                            columns: Array(repeating: GridItem.init(.flexible()), count: 4))
+                    }
                 }
                 .navigationTitle("Lettery")
                 
@@ -46,6 +53,20 @@ struct SettingsView: View {
                     Label("Support", systemImage: "link.circle.fill")
                         .disabled(true)
                 }
+            }
+        }
+        .onAppear(perform: loadConverter)
+    }
+    
+    private func loadConverter() {
+        // TODO: Check if the file is in the document directory, and if it isn't, get it from the bundle
+        if let url = Bundle.main.url(forResource: converterName, withExtension: "json"),
+           let data = try? Data(contentsOf: url) {
+            let decoder = JSONDecoder()
+            if let jsonData = try? decoder.decode(Converter.self, from: data) {
+                self.converter = jsonData
+            } else {
+                print("Couldn't decode file")
             }
         }
     }
